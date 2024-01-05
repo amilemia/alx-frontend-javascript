0465d0a87b73e8d89fe8cb9fbb6d47c6fb765872
Amilemia <aminemadah2@gmail.com>
2024-01-05 04:01:39 +0100

    Fix

--- 0x02-ES6_classes/5-building.js
+++ 0x02-ES6_classes/5-building.js
@@ -1,23 +1,17 @@
 export default class Building {
   constructor(sqft) {
-    if (this.constructor !== Building && !this.evacuationWarningMessage) {
-      throw new Error('Class extending Building must override evacuationWarningMessage');
+    if (
+      new.target !== Building && typeof this.evacuationWarningMessage !== 'function'
+    ) {
+      throw new Error(
+        'Class extending Building must override evacuationWarningMessage',
+      );
     }
     this._sqft = sqft;
   }
 
+  // sqft getter
   get sqft() {
     return this._sqft;
-  }
-
-  set sqft(value) {
-    if (typeof value !== 'number') {
-      throw new TypeError('sqft must be a number');
-    }
-    this._sqft = value;
-  }
-
-  evacuationWarningMessage() {
-    return `The building with ${this._sqft} square feet is being evacuated.`;
   }
 }
