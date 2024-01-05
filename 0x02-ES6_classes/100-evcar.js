85ce94af3565365386d213d2d1131b6366bfb925
Amilemia <aminemadah2@gmail.com>
2024-01-05 03:44:12 +0100

    Task 11

---
+++ 0x02-ES6_classes/100-evcar.js
@@ -0,0 +1,15 @@
+import Car from './10-car';
+
+class EVCar extends Car {
+  constructor(brand, motor, color, range) {
+    super(brand, motor, color);
+
+    this._range = range;
+  }
+
+  cloneCar() {
+    return new Car(this.brand, this.motor, this.color, this._range);
+  }
+}
+
+export default EVCar;
